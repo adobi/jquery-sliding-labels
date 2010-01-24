c6b514b681a0b8cf42d336dbf8621830faab9e32
Attila Dobi <hello@adobi.hu>
2010-01-24 16:08:59 +0200

    horizontal sliding ok

--- jquery.slidingLabels.js
+++ jquery.slidingLabels.js
@@ -3,10 +3,76 @@
 	$.fn.slidingLabels = function(options) {
 		
 		options = $.extend({}, $.fn.slidingLabels.defaults, options);
+		
+		var cssIn = cssOut = {
+			position: 'relative'
+		};
+				
+		
+		var current, prev = 0;
+		
+		return this.each(function() {
+			
+			var self = $(this);
+			var labels = self.find('label');
+
+			labels.each(function(i, label) {
+				
+				switch(options.direction) {
+					case 'vertical':
+					
+						break;
+					case 'horizontal':
+						cssIn.left = $(label).width() + 10;
+						cssIn.opacity = 0.6;
+						break;
+					default:
+				}
+				
+				$(label).css(cssIn);
+			});
+			
+			self.find('input[type=text]').each(function(i, input) {
+				
+				$(input).focusin(function() {
+					switch(options.direction) {
+						case 'horizontal':
+							
+							cssOut.left = 0;
+							cssOut.opacity = 1;
+							break;
+						case 'vertical':
+						
+							break;
+						default:
+					}
+					$(labels[i]).animate(cssOut);
+					current = i;
+					//console.log('focusin '+current);
+				})
+				.focusout(function() {
+					switch(options.direction) {
+						case 'horizontal':
+							
+							cssIn.left = $(labels[current]).width() + 10;
+							cssIn.opacity = 0.6;
+							break;
+						case 'vertical':
+						
+							break;
+						default:
+					}
+					$(labels[current]).animate(cssIn);
+					//console.log('focusout '+current);
+				});
+			});
+			
+			
+		});
 	};
 	
 	$.fn.slidingLabels.defaults = {
-		
+		direction:'horizontal'
 	};
 	
 })(jQuery);
