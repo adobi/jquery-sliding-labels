7e4914092b8b04aba31ae22715d4e2b3c32ac702
Attila Dobi <hello@adobi.hu>
2010-01-24 14:30:45 +0200

    basic setup

---
+++ jquery.slidingLabels.js
@@ -0,0 +1,12 @@
+(function($) {
+	
+	$.fn.slidingLabels = function(options) {
+		
+		options = $.extend({}, $.fn.slidingLabels.defaults, options);
+	};
+	
+	$.fn.slidingLabels.defaults = {
+		
+	};
+	
+})(jQuery);
